a42c5aab63a1cbd9cc501990751d80aefd7b0080
gavizet <gavizet@student.42.fr>
2023-05-12 17:07:04 +0200

    Tested manually, not worth bothering with deep tests.

--- tests/Module_00/test_ex06.py
+++ tests/Module_00/test_ex06.py
@@ -1,10 +1,2 @@
 import pytest
 from Module_00.ex_06.recipe import handle_cookbook
-
-PARAMS = [
-    (),
-]
-
-@pytest.mark.parametrize("user_input, expected", PARAMS)
-def test_recipe(user_input, expected):
-    pass
--- tests/Module_00/test_ex09.py
+++ tests/Module_00/test_ex09.py
@@ -1,11 +1,2 @@
 import pytest
-from io import StringIO
 from Module_00.ex_09.guess import guess_number
-
-PARAMS = [
-    (),
-]
-
-@pytest.mark.parametrize("args, expected", PARAMS)
-def test_guess_number(args, expected):
-    guess_number(42, 0)
