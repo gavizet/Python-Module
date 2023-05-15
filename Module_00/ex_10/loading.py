8fc30ce4a85bb91719ba06f5307e1d2fa1846152
gavizet <gavizet@student.42.fr>
2023-05-15 11:33:59 +0200

    M0E10 small formating update.

--- Module_00/ex_10/loading.py
+++ Module_00/ex_10/loading.py
@@ -15,13 +15,11 @@ def ft_progress(lst):
       lst: The list of elements that we want to iterate over and track progress for.
             It is a range of numbers.
     """
-    current_iter = 0
     total_iterations = len(lst)
     bar_len = 20
     start_time = time()
-    for element in lst:
-        current_iter += 1
-        percentage_completed = current_iter / total_iterations
+    for count, element in enumerate(lst, 1):
+        percentage_completed = count / total_iterations
         # How many '=' we will print to visualize progress. 20 / percentage
         len_progress_bar = int(bar_len * percentage_completed)
         bar_done = "="*len_progress_bar
@@ -29,16 +27,16 @@ def ft_progress(lst):
         bar_left = " "*(bar_len - len_progress_bar)
         # Time passed since we launched the program for the first time
         elapsed_time = time() - start_time
-        # Time it takes to do one full loop of our generator function
-        operation_time = elapsed_time / current_iter
+        # Average time it takes to do one full loop of our generator function
+        operation_time = elapsed_time / count
         # Time left before end of the progress bar's execution
-        eta = operation_time * (total_iterations - current_iter)
+        eta = operation_time * (total_iterations - count)
         # Print f-string to stdout with a carriage return
         print(
-            f"ETA: {eta:.2f}s"
+            f"ETA: {eta:.2f}s "
             f"[{percentage_completed:.2%}]"
-            f"[{bar_done:s}>{bar_left:s}]"
-            f"{current_iter:d}/{total_iterations:d}"
+            f"[{bar_done:s}>{bar_left:s}] "
+            f"{count:d}/{total_iterations:d}"
             f" | elapsed time {elapsed_time:.2f}s",
             end="\r", file=sys.stdout, flush=True)
         # Returns our iteratable generator object
