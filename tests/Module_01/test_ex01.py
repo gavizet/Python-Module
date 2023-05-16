7748d4feed306eb62cac7e3c52961fc5180b4b6a
gavizet <gavizet@student.42.fr>
2023-05-16 12:44:06 +0200

    M01E01 Tests started

---
+++ tests/Module_01/test_ex01.py
@@ -0,0 +1,23 @@
+"""Simple tests with print and asserts for Module_01/ex_01/game.py"""
+#import sys
+#sys.path.append('Module_01/ex_01')
+from Module_01.ex_01.game import Stark
+
+# To test, go into project root folder and use :
+# 'python tests/Module_01/test_ex01.py'
+
+if __name__ == "__main__":
+    arya = Stark("Arya")
+    print(arya.__dict__)
+
+    print(f"Name is {arya.first_name} {arya.family_name}")
+    # Prints the house words
+    print("Arya's House Words are :", end='\r')
+    arya.print_house_words()
+
+    # True
+    print(f"Is Arya alive : {arya.is_alive}")
+    arya.die()
+    print("Arya died")
+    # False
+    print(f"Is Arya alive : {arya.is_alive}")
