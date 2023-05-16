1a311d996708234db3432b78738f93d97c481ae2
gavizet <gavizet@student.42.fr>
2023-05-16 14:24:59 +0200

    M01E01: Changed docstrings, exercice finished.

--- Module_01/ex_01/game.py
+++ Module_01/ex_01/game.py
@@ -2,21 +2,21 @@
 
 
 class GotCharacter:
-    """Base class with 2 attributes on __init__"""
+    """Generic class representing a Game of Thrones character"""
 
-    def __init__(self, first_name, is_alive=True) -> None:
+    def __init__(self, first_name: str = None, is_alive: bool = True) -> None:
         self.first_name = first_name
         self.is_alive = is_alive
 
 
 class Stark(GotCharacter):
-    """Sublass Stark that inherits from GotCharacter and it's __init__ method
+    """Represents the Stark family
 
     Args:
         GotCharacter (class): the parent Class
     """
 
-    def __init__(self, first_name=None, is_alive=True) -> None:
+    def __init__(self, first_name: str = None, is_alive: bool = True) -> None:
         super().__init__(first_name=first_name, is_alive=is_alive)
         self.family_name = "Stark"
         self.house_words = "Winter is Coming"
