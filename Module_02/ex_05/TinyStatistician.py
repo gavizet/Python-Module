a0e4012699b1dfbf469996b0726b64dd21366826
gavizet <gavizet@student.42.fr>
2023-05-24 17:11:04 +0200

    M02E05 start

---
+++ Module_02/ex_05/TinyStatistician.py
@@ -0,0 +1,23 @@
+"""Initiation to very basic statistic notions
+We don't have to protect our program against non-valid inputs"""
+
+
+class TinyStatistician:
+
+    def __init__(self):
+        pass
+
+    def mean(lst):
+        pass
+
+    def median(lst):
+        pass
+
+    def quartiles(lst):
+        pass
+
+    def var(lst):
+        pass
+
+    def std(lst):
+        pass
