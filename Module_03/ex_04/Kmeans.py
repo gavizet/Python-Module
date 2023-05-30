293ecbe1a9fd9e5751b34496427d32f9e00f1f54
gavizet <gavizet@student.42.fr>
2023-05-30 17:25:53 +0200

    M03E04 add doc

--- Module_03/ex_04/Kmeans.py
+++ Module_03/ex_04/Kmeans.py
@@ -3,6 +3,13 @@ import sys
 import argparse
 import numpy as np
 from Module_02.ex_03.csvreader import CsvReader
+from Module_02.ex_05.TinyStatistician import TinyStatistician
+from Module_03.ex_00.NumPyCreator import NumPyCreator
+
+# Distance metrics : https://medium.com/@kunal_gohrani/different-types-of-distance-metrics-used-in-machine-learning-e9928c5e26c7
+# Python code for distance metrics : https://machinelearningmastery.com/distance-measures-for-machine-learning/
+# K-means explanation : https://stanford.edu/~cpiech/cs221/handouts/kmeans.html
+# K-means implementation : https://machinelearningmastery.com/tutorial-to-implement-k-nearest-neighbors-in-python-from-scratch/
 
 
 class KmeansClustering:
@@ -11,6 +18,9 @@ class KmeansClustering:
         self.ncentroid = ncentroid  # number of centroids
         self.max_iter = max_iter  # number of max iterations to update the centroids
         self.centroids = []  # values of the centroids
+
+    def get_dist(self):
+        pass
 
     def fit(self, X):
         """
@@ -48,8 +58,13 @@ if __name__ == "__main__":
     if args.ncentroid < 1 or args.max_iter < 1:
         print("Error : ncentroid and max_iter have to be positive")
     kmeans = KmeansClustering(ncentroid=args.ncentroid, max_iter=args.max_iter)
+    npc = NumPyCreator()
     with CsvReader(args.filepath, header=True) as file:
-        file_data = np.array(file.getdata())
-        print(file_data)
+        file_header = np.array(file.getheader())
+        file_data = npc.from_list(file.getdata())
+        print(file_data[len(file_data) - 3:])
+        print(file_header)
+
+
 # Usage :
 # python Module_03/ex_04/Kmeans.py -filepath="Module_03/ex_04/solar_system_census.csv" -ncentroid=4 -max_iter=3
