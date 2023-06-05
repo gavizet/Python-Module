c7038480934539716bebf5d3041685cfb3e12500
gavizet <gavizet@student.42.fr>
2023-06-05 15:41:22 +0200

    M04E00 done and tested. Updated tests and added docstrings.

--- Module_04/ex_00/FileLoader.py
+++ Module_04/ex_00/FileLoader.py
@@ -4,40 +4,56 @@ import pandas as pd
 
 
 class Fileloader:
+    """ Loads and displays a pandas dataframe """
 
     def __init__(self):
         pass
 
-    def is_valid_file(self, path) -> bool:
-        if path is None:
-            return False
-        if not isinstance(path, str):
-            return False
-        return True
+    @staticmethod
+    def load(path):
+        """ Loads a CSV file from a path and returns the data as a pandas dataframe,
 
-    def load(self, path):
-        if not self.is_valid_file(path):
-            return "Not a valid file, try again"
+        Args:
+          path (str): represents the file path of the CSV file that needs to be loaded.
+
+        Returns:
+          events (pd.DataFrame): contains the data from the CSV file
+          None: if an error was found
+        """
+        events = None
         try:
             events = pd.read_csv(path)
             print(f"Loading dataset of dimensions"
                   f" {events.shape[0]} x {events.shape[1]}")
-            return events
         except pd.errors.EmptyDataError:
             print("Empty csv file!")
         except (FileNotFoundError, ValueError):
             print("Csv file not found")
+        return events
 
-    def display(self, dataframe, nrows) -> None:
+    @staticmethod
+    def display(dataframe, nrows) -> None:
+        """Dispays the first/last nrows of a pandas dataframe passed as argument
+
+        Args:
+            dataframe (pd.DataFrame): a pandas dataframe
+            nrows (int): number of rows to display, can be an int of any value
+        """
         if not isinstance(nrows, int):
-            return "Please input a valid number indicating how many rows you want to display"
-        if nrows < 0 and nrows > -dataframe.shape[0]:
-            print(dataframe.tail(-nrows))
-        elif nrows > 0 and nrows < dataframe.shape[0]:
-            print(dataframe.head(nrows))
+            print("Please enter a valid int")
+        elif not isinstance(dataframe, pd.DataFrame):
+            print("Not a valid pandas dataframe")
+        elif nrows > dataframe.shape[0] or -nrows > dataframe.shape[0]:
+            print(f"nrows higher than dataframe of shape : {dataframe.shape}")
+        else:
+            if nrows < 0:
+                print(dataframe.tail(-nrows))
+            elif nrows > 0:
+                print(dataframe.head(nrows))
 
 
-if __name__ == "__main__":
+def tests():
+    """ Some simples tests for FileLoader class """
     loader = Fileloader()
     print("==== TEST LOADER ====")
     print("--- Does not exist test ---")
@@ -51,9 +67,9 @@ if __name__ == "__main__":
     print()
 
     print("==== TEST DISPLAY ====")
-    print("--- Random string not valid test, should output nothing ---")
+    print("--- Random string not valid test, should output error message ---")
     loader.display(data, "lol")
-    print("--- Int higher than filesize test, sould output nothing ---")
+    print("--- Int higher than filesize test, sould output error message ---")
     loader.display(data, 3000000000)
     print("--- Zero rows valid test, should output nothing ---")
     loader.display(data, 0)
@@ -61,3 +77,7 @@ if __name__ == "__main__":
     loader.display(data, 3)
     print("--- Negative int valid test ---")
     loader.display(data, -3)
+
+
+if __name__ == "__main__":
+    tests()
