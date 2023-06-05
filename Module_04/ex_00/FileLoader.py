9d029d5043bbd2fe956d101cad454bb30bd4a481
gavizet <gavizet@student.42.fr>
2023-06-05 15:22:05 +0200

    M04E00 done and tested.

--- Module_04/ex_00/FileLoader.py
+++ Module_04/ex_00/FileLoader.py
@@ -20,19 +20,44 @@ class Fileloader:
             return "Not a valid file, try again"
         try:
             events = pd.read_csv(path)
-            print(events.shape)
+            print(f"Loading dataset of dimensions"
+                  f" {events.shape[0]} x {events.shape[1]}")
             return events
-
+        except pd.errors.EmptyDataError:
+            print("Empty csv file!")
         except (FileNotFoundError, ValueError):
-            print("Not a valid file path, try again")
+            print("Csv file not found")
 
     def display(self, dataframe, nrows) -> None:
         if not isinstance(nrows, int):
             return "Please input a valid number indicating how many rows you want to display"
+        if nrows < 0 and nrows > -dataframe.shape[0]:
+            print(dataframe.tail(-nrows))
+        elif nrows > 0 and nrows < dataframe.shape[0]:
+            print(dataframe.head(nrows))
 
 
 if __name__ == "__main__":
     loader = Fileloader()
+    print("==== TEST LOADER ====")
+    print("--- Does not exist test ---")
+    data = loader.load("does_not_exist.csv")
+    print("--- Random string test ---")
+    data = loader.load("lol")
+    print("--- Empty file test ---")
+    data = loader.load("Module_04/ex_00/empty_file.csv")
+    print("--- Valid CSV test ---")
     data = loader.load("Module_04/ex_00/athlete_events.csv")
-    # print(data)
-    # loader.display(data, 3)
+    print()
+
+    print("==== TEST DISPLAY ====")
+    print("--- Random string not valid test, should output nothing ---")
+    loader.display(data, "lol")
+    print("--- Int higher than filesize test, sould output nothing ---")
+    loader.display(data, 3000000000)
+    print("--- Zero rows valid test, should output nothing ---")
+    loader.display(data, 0)
+    print("--- Positive int valid test ---")
+    loader.display(data, 3)
+    print("--- Negative int valid test ---")
+    loader.display(data, -3)
