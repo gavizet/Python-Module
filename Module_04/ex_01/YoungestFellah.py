0bcc6c8ec168f85cce47bdd2394b444d67c5fdcf
gavizet <gavizet@student.42.fr>
2023-06-05 16:59:43 +0200

    M04E01 done and tested. Update indexing for performance.

--- Module_04/ex_01/YoungestFellah.py
+++ Module_04/ex_01/YoungestFellah.py
@@ -2,6 +2,9 @@
 import pandas as pd
 from Module_04.ex_00.FileLoader import Fileloader
 
+# See this stackoverflow page for indexing performance with pandas :
+# https://stackoverflow.com/questions/17071871/how-do-i-select-rows-from-a-dataframe-based-on-column-values
+
 
 def youngest_fellah(dataframe: pd.DataFrame, year: int):
     """ Finds the youngest man & woman who took parts in the Olympics for a given year
@@ -18,10 +21,12 @@ def youngest_fellah(dataframe: pd.DataFrame, year: int):
     if not isinstance(year, int):
         return "Year has to be an int"
     result = {'f': float("nan"), 'm': float("nan")}
-    youngest_man = dataframe.loc[(dataframe["Year"] == year)
-                                 & (dataframe["Sex"] == 'M'), "Age"]
-    youngest_woman = dataframe.loc[(dataframe["Year"] == year)
-                                   & (dataframe["Sex"] == 'F'), "Age"]
+    man_mask = (dataframe["Year"].values == year) & \
+        (dataframe["Sex"].values == 'M')
+    female_mask = (dataframe["Year"].values == year) & \
+        (dataframe["Sex"].values == 'F')
+    youngest_man = dataframe.loc[man_mask, "Age"]
+    youngest_woman = dataframe.loc[female_mask, "Age"]
     result['m'] = youngest_man.min()
     result['f'] = youngest_woman.min()
     return result
@@ -33,22 +38,22 @@ def tests():
     data = loader.load("Module_04/ex_00/athlete_events.csv")
 
     print(youngest_fellah(data, "test"))
-    # output is: an error message
+    # expected output is: an error message
 
     print(youngest_fellah("test", 2002))
-    # output is: an error message
-
-    print(youngest_fellah(data, 1992))
-    # output is: "{'f': 12.0, 'm': 11.0}"
-
-    print(youngest_fellah(data, 2004))
-    # output is: "{'f': 13.0, 'm': 14.0}"
-
-    print(youngest_fellah(data, 2010))
-    # output is: "{'f': 15.0, 'm': 15.0}"
+    # expected output is: an error message
 
     print(youngest_fellah(data, 2003))
-    # output is: "{'f': nan, 'm': nan}"
+    # expected output is: "{'f': nan, 'm': nan}"
+
+    print(youngest_fellah(data, 1992))
+    # expected output is: "{'f': 12.0, 'm': 11.0}"
+
+    print(youngest_fellah(data, 2004))
+    # expected output is: "{'f': 13.0, 'm': 14.0}"
+
+    print(youngest_fellah(data, 2010))
+    # expected output is: "{'f': 15.0, 'm': 15.0}"
 
 
 if __name__ == "__main__":
