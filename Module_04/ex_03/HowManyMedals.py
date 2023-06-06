793932c46cc725b550e804e5f415446579cd62c5
gavizet <gavizet@student.42.fr>
2023-06-06 14:03:46 +0200

    M04E03 done and tested. Updated for performance.

--- Module_04/ex_03/HowManyMedals.py
+++ Module_04/ex_03/HowManyMedals.py
@@ -2,7 +2,14 @@
 of dictionaries giving the number and type of medals for each year during which the
 participant won medals. """
 from Module_04.ex_00.FileLoader import Fileloader
+import pandas as pd
 
+
+# See this article about performance when iterating over panda Dataframes :
+# https://towardsdatascience.com/efficiently-iterating-over-rows-in-a-pandas-dataframe-7dd5f9992c01
+# And this stackoverflow post
+# https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas
+# TLDR : DO NOT iterate over dataframe rows, it is an anti pattern. Vectorize.
 
 def how_many_medals(dataframe, name) -> dict:
     """Gets  the number and type of medals for each year during which the participant won medals.
@@ -16,20 +23,22 @@ def how_many_medals(dataframe, name) -> dict:
         medal won. The innermost values correspond to the number of medals of a given type 
         won for a given year.
     """
-    mask = dataframe['Name'].values == name
-    dataframe = dataframe.loc[mask, ['Year', 'Medal']]
-    result = {}
-    for index, row in dataframe.iterrows():
-        if row['Year'] not in result:
-            result[row['Year']] = {'G': 0, 'S': 0, 'B': 0}
-        if row['Medal'] == 'Gold':
-            result[row['Year']]['G'] += 1
-        elif row['Medal'] == 'Silver':
-            result[row['Year']]['S'] += 1
-        elif row['Medal'] == 'Bronze':
-            result[row['Year']]['B'] += 1
-        else:
-            pass
+    name_mask = dataframe['Name'].values == name
+    # Reduce dataframe to only contain the Year and Medal columns
+    dataframe = dataframe.loc[name_mask, ['Year', 'Medal']]
+    # Returns list with each year our 'name' participated in
+    participation_years = dataframe['Year'].unique()
+    # Create dict based on the participation_years list
+    result = dict.fromkeys(participation_years)
+    medals = ['Gold', 'Silver', 'Bronze']
+    # Iterate though our list of years
+    for year in participation_years:
+        result[year] = {'G': 0, 'S': 0, 'B': 0}
+        # Fill 'medal' key based on how many the candidate got for that specific year
+        for medal in medals:
+            medal_mask = (dataframe['Year'] == year) & (
+                dataframe['Medal'] == medal)
+            result[year][medal[0]] = len(dataframe[medal_mask])
     return result
 
 
